3972ac57fea90bb8ff05eaed6b85ff75163146a3
Konstantin Lyssenko <lyskos97@gmail.com>
2019-11-01 16:28:48 +0600

    feat(*): add material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { LayoutComponent } from './components/layout/layout.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LayoutComponent } from "./components/layout/layout.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
