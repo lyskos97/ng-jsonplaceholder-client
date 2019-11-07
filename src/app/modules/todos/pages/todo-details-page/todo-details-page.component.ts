7208703df4c7a9da88784e1d5a79d60c60611dfe
Konstantin Lyssenko <lyskos97@gmail.com>
2019-11-07 12:20:53 +0600

    feat(todos): todo details page

--- src/app/modules/todos/pages/todo-details-page/todo-details-page.component.ts
+++ src/app/modules/todos/pages/todo-details-page/todo-details-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo-details-page',
@@ -6,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-details-page.component.css']
 })
 export class TodoDetailsPageComponent implements OnInit {
+  todoId: string = this.route.snapshot.params['id'];
+  todo$ = this.todoService.getTodo(this.todoId);
 
-  constructor() { }
+  constructor(private todoService: TodoService, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
